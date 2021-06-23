c067adac37be7a91bc9bbe3d05d53d9b32afa4a9
Jakub Bednarski <64811286+SuperrMurlocc@users.noreply.github.com>
2021-06-24 01:50:07 +0200

    Update randomSongPlayer.zsh

--- randomSongPlayer.zsh
+++ randomSongPlayer.zsh
@@ -1,31 +1,29 @@
 function play_music {
-  numberOfPlaylists=$(osascript -e 'tell application "Music"
-    activate
-    get count of playlists
-  end tell')
+  numberOfPlaylists=$(osascript -e "
+    tell application \"Music\"
+        activate
+        get count of playlists
+    end tell
+    ")
 
   playlistNumber=$(jot -r 1  1 $(($numberOfPlaylists - 1)))
 
-  touch randomPlaylistSongCounter
-  echo tell application \"Music\" >> randomPlaylistSongCounter
-  echo get count of tracks of playlist $playlistNumber >> randomPlaylistSongCounter
-  echo end tell >> randomPlaylistSongCounter
-  numberOfSongs=$(osascript randomPlaylistSongCounter)
-  rm randomPlaylistSongCounter
+  numberOfSongs=$(osascript -e "
+    tell application \"Music\"
+        get count of tracks of playlist $playlistNumber
+    end tell
+    ")
 
   songNumber=$(jot -r 1  1 $(($numberOfSongs - 1)))
 
-  touch randomSongPlayer
-  echo tell application \"Music\" >> randomSongPlayer
-  echo play track $songNumber of playlist $playlistNumber >> randomSongPlayer
-  echo end tell >> randomSongPlayer
-  osascript randomSongPlayer
+  osascript -e "
+    tell application \"Music\"
+        play track $songNumber of playlist $playlistNumber
+    end tell"
   if [ $? != 0 ]
   then
-    rm randomSongPlayer
     return 1
   else
-    rm randomSongPlayer
     return 0
   fi
 }
